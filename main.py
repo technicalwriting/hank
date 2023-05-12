e268ecf269575f5196dd7bd0324f185cee77885d
Kayce Basques <kaycebasques@gmail.com>
2023-05-12 14:27:08 -0700

    Add transformation logic

---
+++ main.py
@@ -0,0 +1,40 @@
+from glob import glob
+from json import load
+from os import getcwd
+from jsonlines import open as jsonl_open
+
+def get_config():
+    cwd = getcwd()
+    config_path = f'{cwd}/config.json'
+    with open(config_path, 'r') as f:
+        config = load(f)
+    return config
+
+def transform():
+    out = []
+    config = get_config()
+    stop_sequence = config['stop_sequence']
+    rules = config['rules']
+    cwd = getcwd()
+    for rule in rules:
+        data_paths = glob(f'{cwd}/training/{rule}/*')
+        data_paths.sort(reverse=True)
+        for index, data_path in enumerate(data_paths):
+            if index % 2 == 1:
+                continue
+            before_path = data_path
+            with open(before_path, 'r') as f:
+                before = f.read()
+            after_path = data_paths[index + 1]
+            with open(after_path, 'r') as f:
+                after = f.read()
+            input = f'{before}{stop_sequence}'
+            # OpenAI output should start with a space:
+            # https://platform.openai.com/docs/guides/fine-tuning/data-formatting
+            output = f' {after}'
+            out.append({'prompt': input, 'completion': output})
+    with jsonl_open(f'{cwd}/training.jsonl', 'w') as jsonl_writer:
+        jsonl_writer.write_all(out)
+
+if __name__ == '__main__':
+    transform()
