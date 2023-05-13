d5de244a7c01413690b10c705b87ccf13fbf5ddd
Kayce Basques <kaycebasques@gmail.com>
2023-05-13 09:28:57 -0700

    Start adding scripts

---
+++ hank.sh
@@ -0,0 +1,12 @@
+function bootstrap {
+  python3 -m venv venv
+  source venv/bin/activate
+  python3 -m pip install -r requirements.txt
+  deactivate
+}
+
+function transform {
+  source venv/bin/activate
+  python3 main.py transform
+  deactivate
+}
